2581f30b7acfbc0e4f02190b4ac63977f0375883
Priya Bahuguna <bahugunapriya5@gmail.com>
2026-07-12 23:45:41 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.priya;
+import java.util.Scanner;
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int n1 = sc.nextInt();
+        System.out.println("Enter the second number");
+        int n2 = sc.nextInt();
+        int sum = n1 + n2;
+        System.out.println("The sum is " + sum);
+        sc.close();
+    }
+}
