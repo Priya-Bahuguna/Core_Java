320456c0269ab419b59f889ab404dcc1600e4efd
Priya Bahuguna <bahugunapriya5@gmail.com>
2026-09-07 15:26:05 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+package com.priya;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        byte b = 123;
+        short s = 234;
+        int i =6754;
+        long l = 1234567890L;
+        float f = 23.8f;
+        double d = 1234.23;
+        char c = 'a';
+        boolean bo= true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(bo);
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,17 @@
+package com.priya;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name ="Priya";
+        int age = 20;
+        float height = 162f;
+        String college = "Graphic Era Hill University";
+        boolean isStudent = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(college);
+        System.out.println(isStudent);
+
+    }
+}
