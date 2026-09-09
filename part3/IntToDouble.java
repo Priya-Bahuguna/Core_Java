809470190c01be38495c8d545fbc3080da428be9
Priya Bahuguna <bahugunapriya5@gmail.com>
2026-09-09 10:54:34 +0530

    Add files via upload

---
+++ part3/IntToDouble.java
@@ -0,0 +1,34 @@
+package com.part3;
+
+public class IntToDouble {
+    public static void main(String[] args) {
+        int a =34;
+        double b = a;
+        System.out.println(b);
+        // double to int
+        double c = 23.4;
+        int d = (int) c;
+        System.out.println(d);
+
+        // take a decimal no. and print its integer parts using casting
+        double s= 78.4;
+        int m = (int) s;
+        System.out.println(m);
+
+        //convert int into float
+        int r = 22;
+        float f = (float)r;
+        System.out.println(f);
+
+        //char into int
+        char ch='A';
+        int w =  ch;
+        System.out.println(w);
+
+        //int gto char
+        int k=66;
+        char cp=(char)k;
+        System.out.println(cp);
+
+    }
+}
